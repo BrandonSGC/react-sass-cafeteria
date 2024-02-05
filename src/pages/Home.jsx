047f8935eebd2a05b4716e4402d9eb1c1081e18e
BrandonSGC <brangmz20@gmail.com>
2024-02-05 11:47:17 -0600

    Header finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <header className="header header-inicio">
+        <div className="contenido-header contenedor">
+          <div className="barra">
+            <Link to="/" className="logo">
+              <img src="/icons/logo.svg" alt="Logo" />
+            </Link>
+
+            <nav className="nav-principal">
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/"
+              >
+                Inicio
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/nosotros"
+              >
+                Nosotros
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/proceso"
+              >
+                Proceso
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/menu"
+              >
+                Menú
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/galeria"
+              >
+                Galería
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => `${isActive && "activo"}`}
+                to="/contacto"
+              >
+                Contacto
+              </NavLink>
+            </nav>
+          </div>
+
+          <div className="text-header">
+            <h1>Disfruta una deliciosa taza de café</h1>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,7 +5,13 @@ import { Home } from '../pages/'
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<Home />}/>
+      <Route path="/process" element={<Home />}/>
+      <Route path="/galery" element={<Home />}/>
+      <Route path="/menu" element={<Home />}/>
+      <Route path="/contact" element={<Home />}/>
+      <Route path="/*" element={<Home />}/>
     </Routes>
   )
 }
