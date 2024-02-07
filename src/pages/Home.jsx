017c4295265c54b1172e3e2fdc23530bb43f673b
BrandonSGC <brangmz20@gmail.com>
2024-02-07 14:32:40 -0600

    Sobre Nosotros

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export const Home = () => {
+  
+
   return (
     <>
       <header className="header header-inicio">
         <div className="contenido-header contenedor">
           <div className="barra">
             <Link to="/" className="logo">
-              <img src="/icons/logo.svg" alt="Logo" />
+              <img src="/images/logo.svg" alt="Logo" />
             </Link>
 
             <nav className="nav-principal">
@@ -56,6 +58,37 @@ export const Home = () => {
           </div>
         </div>
       </header>
+
+      <main className="contenedor contenido-principal text-center">
+        <section className="conoce">
+          <h2>
+            <span>Conoce más</span> Sobre Nosotros
+          </h2>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            voluptatibus vitae autem. Praesentium mollitia sapiente sint
+            aspernatur sunt qui fugit totam? Unde blanditiis doloremque,
+            aspernatur ad porro repudiandae impedit saepe.
+          </p>
+        </section>
+
+        <section className="iconos">
+          <ul className="listado-iconos">
+            <li>
+              <img src="/images/icono_cafe.svg" alt="icono café" />
+              <p>Café</p>
+            </li>
+            <li>
+              <img src="/images/icono_postre.svg" alt="icono postres" />
+              <p>Postres</p>
+            </li>
+            <li>
+              <img src="/images/icono_te.svg" alt="icono té" />
+              <p>Té</p>
+            </li>
+          </ul>
+        </section>
+      </main>
     </>
   );
 };
